ed6751609406615378dfec73e73bd7ee181e2922
ilya <ilya.five@gmail.com>
2015-01-24 22:27:07 -0300

    Added QT Creator project. Renamed iff to the if_then_else.

--- modules/double/if_then_else.cpp
+++ modules/double/if_then_else.cpp
@@ -33,30 +33,30 @@ namespace scalar
 {
 
 /// If condition holds return fist input, otherwise second
-class iff :
+class if_then_else :
 	public k3d::scalar_source
 {
 	typedef k3d::scalar_source base;
 public:
-	iff(k3d::iplugin_factory& Factory, k3d::idocument& Document) :
+    if_then_else(k3d::iplugin_factory& Factory, k3d::idocument& Document) :
 		base(Factory, Document, _("'if then else' operator.")),
-        m_condition(init_owner(*this) + init_name("condition") + init_label(_("Condition")) + init_description(_("Boolean condition")) + init_value(true)),
-		m_input1(init_owner(*this) + init_name("input1") + init_label(_("Input 1")) + init_description(_("First input float")) + init_value(0.0)),
-		m_input2(init_owner(*this) + init_name("input2") + init_label(_("Input 2")) + init_description(_("Second input float")) + init_value(0.0))
+        m_if(init_owner(*this) + init_name("if") + init_label(_("If")) + init_description(_("Boolean condition")) + init_value(true)),
+        m_then(init_owner(*this) + init_name("then") + init_label(_("Then")) + init_description(_("Then input")) + init_value(0.0)),
+        m_else(init_owner(*this) + init_name("else") + init_label(_("Else")) + init_description(_("Else input")) + init_value(0.0))
 	{
-        m_condition.changed_signal().connect(k3d::hint::converter<
+        m_if.changed_signal().connect(k3d::hint::converter<
 			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_value_slot()));
-		m_input1.changed_signal().connect(k3d::hint::converter<
+        m_then.changed_signal().connect(k3d::hint::converter<
 			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_value_slot()));
-		m_input2.changed_signal().connect(k3d::hint::converter<
+        m_else.changed_signal().connect(k3d::hint::converter<
 			k3d::hint::convert<k3d::hint::any, k3d::hint::none> >(make_update_value_slot()));
 	}
 
 	static k3d::iplugin_factory& get_factory()
 	{
-		static k3d::document_plugin_factory<iff > factory(
+        static k3d::document_plugin_factory<if_then_else > factory(
 			k3d::uuid(0x0338911f, 0x8f1749cf, 0xbeb3c4ee, 0x8967fc4f),
-			"DoubleIff",
+            "DoubleIfThenElse",
 			_("If condition holds return fist input, otherwise second"),
 			"Double",
 			k3d::iplugin_factory::STABLE);
@@ -65,19 +65,19 @@ public:
 	}
 
 private:
-    k3d_data(k3d::bool_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, writable_property, with_serialization) m_condition;
-	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, writable_property, with_serialization) m_input1;
-	k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, writable_property, with_serialization) m_input2;
+    k3d_data(k3d::bool_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, writable_property, with_serialization) m_if;
+    k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, writable_property, with_serialization) m_then;
+    k3d_data(k3d::double_t, immutable_name, change_signal, with_undo, local_storage, no_constraint, writable_property, with_serialization) m_else;
 
 	void on_update_value(k3d::double_t& Output)
 	{
-		Output = m_condition.pipeline_value() ? m_input1.pipeline_value() : m_input2.pipeline_value();
+        Output = m_if.pipeline_value() ? m_then.pipeline_value() : m_else.pipeline_value();
 	}
 };
 
-k3d::iplugin_factory& iff_factory()
+k3d::iplugin_factory& if_then_else_factory()
 {
-	return iff::get_factory();
+    return if_then_else::get_factory();
 }
 
 } //namespace scalar
